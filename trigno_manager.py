153c9925e64657b8e12db02a95f4d5eb59ccb7c9
Zach Seitz <zachmichael14@gmail.com>
2024-11-21 10:23:16 -0600

    Add stream state enum and threaded streaming

--- trigno_manager.py
+++ trigno_manager.py
@@ -1,26 +1,105 @@
+from enum import auto, Enum
 from queue import Queue
+import threading
+import time
 
 from abstract_client import DeviceManager
 from trigno_client import TrignoClient
 
+class StreamState(Enum):
+    STOPPED = auto()
+    RUNNING = auto()
+    PAUSED = auto()
+
+
 class TrignoManager(DeviceManager):
 
-    def __init__(self, client: TrignoClient):
-        self.trigno_client = client
-        self.queue = Queue()
+    def __init__(self, 
+                 client: TrignoClient,
+                 host_ip: str = "10.229.96.105",
+                 ):
+        self.client = client(host_ip)
 
-    async def connect(self):
-        """Connect to the device."""
-        pass
+        self.streamed_data_queue = Queue()
+        self.stream_state = StreamState.STOPPED
+        self.stream_thread = None
 
-    async def start_streaming(self, queue: Queue):
+    def connect(self):
+        """Establish a connection to the Trigno server"""
+        self.client.connect()
+
+    def start_streaming(self):
         """Start streaming data."""
-        pass
 
-    async def stop_streaming(self):
-        """Stop streaming data."""
-        pass
+        # Streaming can only be started if it's currently stopped
+        # This is because resuming after pause does not need to recreate 
+        # a thread
+        if self.stream_state != StreamState.STOPPED:
+            return
+        self.client.start_streaming()
+        
+        self.stream_state = StreamState.RUNNING
+        self.stream_thread = threading.Thread(target=self._stream_data)
+        # self.client.start_streaming()
+        self.stream_thread.start()
 
-    def get_device_name(self):
-        """Return the device name."""
-        pass
+    def pause_streaming(self):
+        if self.stream_state == StreamState.RUNNING:
+            self.stream_state = StreamState.PAUSED
+
+    def resume_streaming(self):
+        if self.stream_state == StreamState.PAUSED:
+            self.stream_state = StreamState.RUNNING
+
+    def stop_streaming(self):
+        """
+        Stop Trigno sensor data streaming.
+
+        Joins the thread and sends stop command to Trigno server.
+
+        This method requires a particular order of events:
+            1) Set stream state to STOPPED before joining thread or it will 
+            run indefinitely while trying to join thread.
+            2) Join thread before sending stop command to server to avoid a
+            TimeoutError caused by the thread requesting data after the socket is closed.
+            3) Send stop command to server after the above is completed
+        """
+        self.stream_state = StreamState.STOPPED
+
+        # Stream state must be set to STOPPED before joining thread
+        if self.stream_thread:
+            self.stream_thread.join()
+            self.stream_thread = None
+
+        # Thread must be joined before calling client's stop method
+        self.client.stop_streaming()
+
+    def _stream_data(self):
+        while self.stream_state != StreamState.STOPPED:
+            if self.stream_state == StreamState.RUNNING:
+                try:
+                    frame = self.client.receive_emg_frame()
+                    print(frame)
+                    self.streamed_data_queue.put(frame)
+                except Exception as e:
+                    print(f"Streaming Error: {e}")
+                    self.stop_streaming()
+                    break
+
+            else:
+                # Yield CPU time while paused
+                time.sleep(0.01)
+
+if __name__ == "__main__":
+    client = TrignoClient
+    manager = TrignoManager(client)
+    manager.connect()
+    manager.start_streaming()
+    
+    # Example of streaming control
+    time.sleep(2)  # Stream for 5 seconds
+    manager.pause_streaming()
+    time.sleep(2)  # Pause for 2 seconds
+    manager.resume_streaming()
+    time.sleep(2)  # Stream for 3 more seconds
+    manager.stop_streaming()
