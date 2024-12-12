17dffc6960cb30809aac7191ac4c1d5d493321ba
Zach Seitz <zachmichael14@gmail.com>
2024-12-12 14:03:29 -0600

    Add detection window colors

--- src/utils/colors.py
+++ src/utils/colors.py
@@ -15,3 +15,12 @@ class LabColors(Enum):
     @classmethod
     def get_all_colors(cls) -> List[QColor]:
         return [color.value for color in cls]
+
+
+class DetectionWindowColors(Enum):
+    """
+    Plot titles use HTML while detection windows use QColors.
+    To make these apply to both components, hexcodes were used.
+    """
+    PURPLE = "#9232BB" # (146, 51, 187)
+    BLUE = "#00B4D8" # (0, 180, 216)
