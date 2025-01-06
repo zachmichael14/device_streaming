2874dec6e2d8b0cde0a61abbad346978fda8158d
Zach Seitz <zachmichael14@gmail.com>
2025-01-06 14:40:37 -0600

    Only call window and config manager

--- src/main.py
+++ src/main.py
@@ -1,24 +1,26 @@
+import sys
 
-from src.clients.usbamp_client import USBAmpClient
-import pygds as g
+from PySide6.QtWidgets import QApplication, QStyleFactory
 
-# def main():
-    ### USBAMP TEST ###
-    # usb_amp = USBAmpClient()
-    # if not usb_amp.connect():
-    #     return
-    # d = usb_amp.connection
-    
-    
-    # scope = g.Scope(1/d.SamplingRate, title="Channels: %s", ylabel = u"U[μV]")
-    # a = d.GetData(d.SamplingRate//2,scope)
+from src.managers.config_manager import ConfigManager
+from src.managers.window_manager import WindowManager
 
-    # del scope
-    
 
-    # # Cleanup
-    # usb_amp.disconnect()
+# class MainApplicationWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.setWindowTitle("Main Application")
+#         self.central_widget = MainWidget()
+#         self.setCentralWidget(self.central_widget)
 
-# if __name__ == '__main__':
-    # print("main")
-    # main()
+if __name__ == "__main__":
+    app = QApplication([])
+    QApplication.setStyle(QStyleFactory.create("Fusion"))
+
+    config_manager = ConfigManager()
+    window_manager = WindowManager(config_manager)
+
+    config_window = window_manager.create_config_window()
+    config_window.show()
+
+    sys.exit(app.exec())
